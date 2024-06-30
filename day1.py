747619ddba91d0334107792d5853ecaed5b7927a
SawanStha06 <sawanshrewta@gmail.com>
2024-06-30 22:34:37 +0545

    day1

---
+++ day1.py
@@ -0,0 +1,10 @@
+print(5*5)
+print(5/5)
+print(5*5/5)
+print(2+2/2)
+#calculation in print statements
+#comments,Escape and print statement
+print("india world the world cup \n after")
+print("the day is \"drak\"nand to get out of the darkness go to sleep")
+print("hey",6,7,sep="~",end="009\n")
+print("sawan")
