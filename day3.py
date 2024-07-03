184e802d4d0ed5afbd784539a0d3d47d7b10d1b4
SawanStha06 <sawanshrewta@gmail.com>
2024-07-03 22:47:41 +0545

    day3-operators

---
+++ day3.py
@@ -0,0 +1,22 @@
+#operators
+#Addition (+)
+#subtraction(-)
+#multiplication(*)
+#Exponential(**)
+#division(/)
+#modulus(%)
+#floor division(//)
+print(6+12)
+print(6-12)
+print(6*12)
+print(6**2)
+print(12/6)
+print(12//5 )
+#create a calulator capable of performing addition,subtraction,multiplication and division operations on two numbers.
+("\n")
+a=5
+b=10
+print("the value of",a,"+",b,"is:",a+b)
+print("the value of",a,"-",b,"is:",a+b)
+print("the value of",a,"*",b,"is:",a+b)
+print("the value of",a,"/",b,"is:",a+b)
