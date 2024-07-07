8769cf88032064f4cecf35d0c2feec777c7952f7
SawanStha06 <sawanshrewta@gmail.com>
2024-07-07 22:48:45 +0545

    strings in python

---
+++ day6.py
@@ -0,0 +1,25 @@
+#strings
+name='Sawan'
+print("hello ,"+name)
+#multiple line strings
+word="Hi there!,its jarvis"
+print(word)
+#triple quote
+string='''there was a dragon
+who was awaken after hundreds of years.
+"drogon" the dragon'''
+print(string)
+#indexing
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+#print(name[5]) index error
+
+#for loop in string
+print("\n")
+for character in name:
+ print(character)
+
+
