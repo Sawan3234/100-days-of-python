132eca72eb3d8556439df0a8e0a699b941f18d70
SawanStha06 <sawanshrewta@gmail.com>
2024-07-07 22:28:59 +0545

    taking input from user

--- day5.py
+++ day5.py
@@ -1,3 +1,11 @@
 #userinputs
-a=input()
-print(a)
+#calculator asking value from user
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print("addition",(a+b) ,type(a+b))
+print("subtraction",a-b)
+print("multiplication",a*b)
+
+x=input("another number:")
+y=input("other nubmber:")
+print("sum",int (a)+int (b))
