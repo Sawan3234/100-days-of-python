a9f382d9890268e7061a0ada2522226cde21fddc
SawanStha06 <sawanshrewta@gmail.com>
2024-07-07 23:06:18 +0545

    string slicing

---
+++ day7.py
@@ -0,0 +1,17 @@
+#string slicing
+#[first index(0):required lenth index[]]
+script="The dance of the dragons"
+print(len(script))
+print(script[0:7])#string slicing
+#negative slicing
+name='sawan'
+print(len(name))
+print(name[0:len(name)-1])
+
+print(name[-5:-1])
+print(name[1:4])
+ 
+nm='Harry'
+print(nm[-4:-2])
+num="12345"
+print(num[2:5])
